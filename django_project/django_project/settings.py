590f6a7a508d492cc58644f268b28bc609ab2540
Utkarsh Gauniyal <ugauniyal@outlook.com>
2020-12-12 22:11:47 +0530

    add user regitration,login and logout feature

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bucket_list',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'tasks'
+
+LOGIN_URL = 'login'
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from bucket_list import views
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bucket_list.urls')),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
